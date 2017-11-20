11f4b70051a77dc2c85b230a958683c625a51c52
Artyom Alchangyan <alchangyan@mail.ru>
2017-11-20 22:06:02 +0400

    1 hour for create main interface + dinner :)

--- task/src/App.js
+++ task/src/App.js
@@ -7,16 +7,18 @@ class App extends Component {
         <h1>Register account</h1>
         <label htmlFor="firstName">Name</label>
         <input type="text" id="firstName"/>
-        <span className="error errorFirstName"></span>
+        <span className="error errorInput errorFirstName">Name is required</span>
         <label htmlFor="lastName">Last name</label>
         <input type="text" id="lastName"/>
-        <span className="error errorLastName"></span>
+        <span className="error errorInput errorLastName">Name is required</span>
         <label htmlFor="email">Email</label>
         <input type="text" id="email"/>
-        <span className="error errorEmail"></span>
+        <span className="error errorInput errorEmail">Name is required</span>
         <h2>Bank accounts</h2>
-        <span className="error errorBanks">You should provide at least bank account</span>
-        <div className="btn btnBank">+ Add bank account</div>
+        <div className="bankAccounts">
+          <span className="error errorBanks">You should provide at least bank account</span>
+          <div className="btn btnBank">+ Add bank account</div>
+        </div>
         <div className="btn btnSubmit">Submit!</div>
       </div>
     );
