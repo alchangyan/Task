62015cd0a56e9845842e49d6cc5b754f484512b1
Artyom Alchangyan <alchangyan@mail.ru>
2017-11-20 21:42:28 +0400

    created DOM and some styles

--- task/src/App.js
+++ task/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="form">
-        
+        <h1>Register account</h1>
+        <label htmlFor="firstName">Name</label>
+        <input type="text" id="firstName"/>
+        <span className="error errorFirstName"></span>
+        <label htmlFor="lastName">Last name</label>
+        <input type="text" id="lastName"/>
+        <span className="error errorLastName"></span>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email"/>
+        <span className="error errorEmail"></span>
+        <h2>Bank accounts</h2>
+        <span className="error errorBanks">You should provide at least bank account</span>
+        <div className="btn btnBank">+ Add bank account</div>
+        <div className="btn btnSubmit">Submit!</div>
       </div>
     );
   }
